6c61522f7d69d1c97953e24d672c27fab95f8621
Reister Gerald Conde <grldcondezz18@gmail.com>
2025-06-10 13:37:34 +0800

    experimental

--- keep_alive.py
+++ keep_alive.py
@@ -1,6 +1,4 @@
 import os
-import time
-import requests
 from flask import Flask
 from threading import Thread
 
@@ -14,15 +12,6 @@ def run():
     port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
 
-def ping_replit():
-    while True:
-        try:
-            requests.get("https://6e4f5529-9a5d-469e-a825-abc89a4c4566-00-2chl2pt6folb9.sisko.replit.dev/")
-            print("Pinged Replit")
-        except Exception as e:
-            print("Failed to ping Replit:", e)
-        time.sleep(300)
-
 def keep_alive():
-    Thread(target=run).start()
-    Thread(target=ping_replit).start()
+    t = Thread(target=run)
+    t.start()
